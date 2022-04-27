d5961a521c0153a3269835e3b48a1a271f2c04d2
qeoptuuqrro <junhaochen2001@gmail.com>
2022-04-26 20:24:17 -0400

    addding javascripts to navbar, so it can show when u click

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,7 @@
+//navbar toggler
+let navToggler = document.getElementById('navbar-toggler');
+let navCollapse = document.querySelector('.navbar-collapse');
 
+navToggler.addEventListener('click', () => {
+    navCollapse.classList.toggle('showNav');
+});
