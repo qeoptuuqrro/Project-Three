86b7e8cb2f50d0b7145ab0386f28637e2fe77dc5
qeoptuuqrro <junhaochen2001@gmail.com>
2022-04-27 18:02:49 -0400

    finish the javascripts for transition

--- src/js/main.js
+++ src/js/main.js
@@ -5,3 +5,45 @@ let navCollapse = document.querySelector('.navbar-collapse');
 navToggler.addEventListener('click', () => {
     navCollapse.classList.toggle('showNav');
 });
+
+function changeFilterBtn(){
+    filterBtns.forEach((btn) => {
+        btn.classList.remove('btn-active');
+    });
+}
+
+$('home').addClass("active");
+
+
+$("#home").click(function() {
+     $('html, body').animate({
+         scrollTop:        $("home").offset().top-10
+     }, 1000);
+  return false;
+ });
+
+
+ $("#about").click(function() {
+    $('html, body').animate({
+        scrollTop:        $("about").offset().top-10
+    }, 1000);
+ return false;
+});
+
+$("#history").click(function() {
+    $(' html,body') .animate({
+        scrollTop:        $("history").offset().top-112
+    }, 1000);
+ return false;
+});
+
+$("#images").click(function() {
+    $(this).addClass("active");
+       $('html,body ') .animate({
+           scrollTop:        $("images").offset().top-112
+       }, 1000);
+    return false;
+   });
+
+
+
